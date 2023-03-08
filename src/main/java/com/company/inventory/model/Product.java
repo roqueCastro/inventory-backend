dcb7ed8296408bc48b626286fec1b7d52e7d20dc
Roque Castro <rokke1558@gmail.com>
2023-03-07 23:49:33 -0500

    Guardar productos

---
+++ src/main/java/com/company/inventory/model/Product.java
@@ -0,0 +1,43 @@
+package com.company.inventory.model;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="product")
+public class Product  implements Serializable{
+	
+	
+	private static final long serialVersionUID = -7626560619459238044L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private int price;
+	private int account;
+ 
+	@ManyToOne(fetch= FetchType.LAZY)
+	@JsonIgnoreProperties({"hibernateLazyInitalizer", "handler"})
+	private Category category;
+	
+	@Lob
+	@Basic(fetch = FetchType.LAZY)
+	@Column(name="picture", columnDefinition="LONGBLOB")
+	private byte[] picture;
+
+}
